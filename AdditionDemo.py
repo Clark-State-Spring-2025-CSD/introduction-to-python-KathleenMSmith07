03ab7b2ad5856ab2f635af4b70a2b1fbf346c1c8
KathleenMSmith07 <katiesmith2707@gmail.com>
2025-02-04 13:59:53 -0500

    Update AdditionDemo.py

---
+++ AdditionDemo.py
@@ -0,0 +1,8 @@
+print('please enter 2 whole numbers')
+number = any
+Number1 = int(input(number ))
+Number2 = int(input(number ))
+Sum = Number1 + Number2
+
+answer= f"The sum of {Number1} and {Number2} is {Sum}."
+print(answer)
